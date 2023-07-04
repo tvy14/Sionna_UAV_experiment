dffb509c4911cc00c5668e9a7a2069afe5c7c3ad
邱佳詮 <s87315teve@gmail.com>
2023-07-04 20:27:12 +0800

    fix bug

--- video_streaming/video_tx.py
+++ video_streaming/video_tx.py
@@ -115,8 +115,8 @@ def on_release(key):
         return False
 
 # Collect events until released
-with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
-    listener.join()
+# with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
+#     listener.join()
 
 # ...or, in a non-blocking fashion:
 listener = keyboard.Listener(on_press=on_press,on_release=on_release)
