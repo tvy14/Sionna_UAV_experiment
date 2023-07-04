2091c88781573037f57dd5863baf535c7ff65746
邱佳詮 <s87315teve@gmail.com>
2023-07-04 19:40:03 +0800

    fix bug

--- video_streaming/video_tx.py
+++ video_streaming/video_tx.py
@@ -62,7 +62,7 @@ class CamThread(threading.Thread):
             new_image = np.frombuffer(imgencode, np.uint8)
             new_image=cv2.imdecode(new_image,1)
             psnr=calculate_psnr(frame, new_image)
-            print("PSNR:{:.2f}dB".format(psnr))
+            #print("PSNR:{:.2f}dB".format(psnr))
             #-----calculate psnr end------
             data = np.array(imgencode)
             stringData = data.tostring()
@@ -81,8 +81,53 @@ class CamThread(threading.Thread):
 
 
 
-cam_t=CamThread()
-cam_t.start()
+#cam_t=CamThread()
+#cam_t.start()
+
+
+
+#######################
+
+
+#cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+cam = cv2.VideoCapture(0)
+quality=90
+encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),quality]
+while True:
+    if cv2.waitKey(1) & 0xFF == ord('w'):
+        if quality<=90:
+            quality+=5
+        encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),quality]
+        print("quality:{}".format(quality))
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        if quality>=10:
+            quality-=5
+        encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),quality]
+        print("quality:{}".format(quality))
+
+    ret, frame = cam.read()
+    result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+    #-----calculate psnr-----
+    new_image = np.frombuffer(imgencode, np.uint8)
+    new_image=cv2.imdecode(new_image,1)
+    psnr=calculate_psnr(frame, new_image)
+    #print("PSNR:{:.2f}dB".format(psnr))
+    #-----calculate psnr end------
+    data = np.array(imgencode)
+    stringData = data.tostring()
+
+    #-------PSNR、frame size to json-----
+    json_msg={"PSNR":psnr, "size":len(stringData),'time': time.time()}
+    json_msg=json.dumps(json_msg)
+    sock.send(json_msg.ljust(200).encode('utf-8'))
+    #sock.send( (str(len(stringData)).ljust(16)).encode('utf-8'))
+    sock.send( stringData )
+sock.close()
+cam.release()
+cv2.destroyAllWindows()
+
+
+
 
 
 
